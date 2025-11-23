579109d7d821210836c0550caa94cab8e81e82ef
杨子岩 <yangziyan0708@foxmail.com>
2025-11-23 22:46:31 +0800

    实现游戏功能和界面，添加题库和AI判定接口

---
+++ static/js/mode.js
@@ -0,0 +1,18 @@
+// 点击模式卡片 -> 选中样式切换
+document.querySelectorAll(".mode-option").forEach(opt => {
+    opt.addEventListener("click", () => {
+        document.querySelectorAll(".mode-option").forEach(o => o.classList.remove("selected"));
+        opt.classList.add("selected");
+    });
+});
+
+// 点击 "Start Game"
+function startGame() {
+    const selected = document.querySelector(".mode-option.selected");
+    const mode = selected.dataset.mode;
+
+    // riddle_id 是从 URL 自动传过来的
+    const riddleId = window.location.pathname.split("/")[2];
+
+    window.location.href = `/game/${riddleId}/${mode}`;
+}
